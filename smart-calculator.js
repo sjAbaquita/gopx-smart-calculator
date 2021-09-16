86043b07349a1e039d64cece8529ec0177207eaa
Seth <sj.abaquita26@gmail.com>
2021-09-16 22:00:57 +0800

    added number formater

--- smart-calculator.js
+++ smart-calculator.js
@@ -134,7 +134,7 @@
 
             (!$.isNumeric(amount) ? value = 0 : value = amount*date_value)
 
-            $('#display-value').html('<small>$</small>'+value.toLocaleString());
+            $('#display-value').html('<small>$</small>'+(value != 0 ? number_format( value, 2, '.', ',' ) : value));
         }
 
         //will change this approach
@@ -161,6 +161,39 @@
                 $('#calculator-progress-bar').find('.calculator-is-pass').removeClass().addClass('calculator-not-pass')
             }
             
+        }
+
+        var number_format = function(number, decimals, decPoint, thousandsSep) {
+            //  JavaScript equivalent to PHP's number_format
+            //  discuss at: https://locutus.io/php/number_format/
+            number = (number + '').replace(/[^0-9+\-Ee.]/g, '')
+            const n = !isFinite(+number) ? 0 : +number
+            const prec = !isFinite(+decimals) ? 0 : Math.abs(decimals)
+            const sep = (typeof thousandsSep === 'undefined') ? ',' : thousandsSep
+            const dec = (typeof decPoint === 'undefined') ? '.' : decPoint
+            let s = ''
+            const toFixedFix = function (n, prec) {
+                if (('' + n).indexOf('e') === -1) {
+                    return +(Math.round(n + 'e+' + prec) + 'e-' + prec)
+                } else {
+                    const arr = ('' + n).split('e')
+                    let sig = ''
+                    if (+arr[1] + prec > 0) {
+                        sig = '+'
+                    }
+                    return (+(Math.round(+arr[0] + 'e' + sig + (+arr[1] + prec)) + 'e-' + prec)).toFixed(prec)
+                }
+            }
+            // @todo: for IE parseFloat(0.55).toFixed(0) = 0;
+            s = (prec ? toFixedFix(n, prec).toString() : '' + Math.round(n)).split('.')
+            if (s[0].length > 3) {
+                s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep)
+            }
+            if ((s[1] || '').length < prec) {
+                s[1] = s[1] || ''
+                s[1] += new Array(prec - s[1].length + 1).join('0')
+            }
+            return s.join(dec)
         }
 
         return this.init();
